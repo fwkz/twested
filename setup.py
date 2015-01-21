266ea8c63d46f0a45053c384a5d6d27ae3a91784
fwkz <f4wkes@gmail.com>
2015-01-21 10:55:21 +0100

    Re-organizing project structure. Added setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='twested',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/fwkz/twested',
+    license='MIT',
+    author='Mariusz Kupidura',
+    author_email='f4wkes@gmail.com',
+    description='Automating web browser actions Twisted style.'
+)
